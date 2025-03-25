67abd298e57785c95dc5b7422558516a3367d333
L-pixela <122270958+L-pixela@users.noreply.github.com>
2025-03-25 15:22:21 +0700

    BLA-202 – Repository and Provider with Future<T> (step1-3)

--- lib/providers/rides_preference_provider.dart
+++ lib/providers/rides_preference_provider.dart
@@ -2,12 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:week_3_blabla_project/model/ride/ride_pref.dart';
+import 'package:week_3_blabla_project/providers/async_value.dart';
 import 'package:week_3_blabla_project/repository/ride_preferences_repository.dart';
 
 class RidesPreferenceProvider extends ChangeNotifier {
   final RidePreferencesRepository repository;
 
-  List<RidePreference> pastPreference = [];
+  late AsyncValue<List<RidePreference>> pastPreference;
 
   RidePreference? _currentPreference;
 
@@ -23,17 +24,40 @@ class RidesPreferenceProvider extends ChangeNotifier {
       // print("Set current pref to $_currentPreference");
       _currentPreference = preference;
       addPreference(preference);
-      repository.addPreference(preference);
       notifyListeners();
     }
   }
 
-  void addPreference(RidePreference preference) {
-    pastPreference.add(preference);
+  Future<void> addPreference(RidePreference preference) async {
+    try {
+      // 1. Add to the Repository
+      await repository.addPreference(preference);
+
+      // 2. Refetch data again
+      await fetchPastPreferences();
+    } catch (e) {
+      pastPreference = AsyncValue.error(e);
+    }
     notifyListeners();
   }
 
-  List<RidePreference> getPastPreferences() {
-    return pastPreference.reversed.toList();
+  Future<void> fetchPastPreferences() async {
+    // 1- Handle loading
+    pastPreference = AsyncValue.loading();
+    notifyListeners();
+
+    try {
+      // 2- Fetch Data
+      List<RidePreference> pastPrefs = await repository.getPastPreferences();
+
+      // 3- Handle Success
+      pastPreference = AsyncValue.success(pastPrefs);
+
+      // 4- Hanles Error
+    } catch (e) {
+      pastPreference = AsyncValue.error(e);
+    }
+
+    notifyListeners();
   }
 }
